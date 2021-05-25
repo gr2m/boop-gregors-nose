7a570b9682c91d3044a8e0bdaf9fe85318dd9e0d
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-05-25 11:17:58 -0700

    fix: do not use OAuth flow in GitHub Action

---
+++ action.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const { Octokit } = require("octokit");
+
+const { boopGregorsNose } = require("./lib/octokit-plugin-boop-gregors-nose");
+
+// register plugin and set default for user agent
+const MyOctokit = Octokit.plugin(boopGregorsNose).defaults({
+  userAgent: "gregors-nose-booper-action",
+});
+
+run();
+
+async function run() {
+  // instantiate `octokit` with the OAuth Device authentication strategy and
+  // credentials for an OAuth app
+  const octokit = new MyOctokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+
+  await octokit.boopGregorsNose();
+}
